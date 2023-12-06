0b624a9524c10415d1365a4d2803e5b7f2c611a4
ismat27 <adegbiteismail27@gmail.com>
2023-12-06 15:07:40 +0100

    update activity ride list content adapter configuration

--- app/src/main/java/com/example/introandroidclass/ScrollableActivity.kt
+++ app/src/main/java/com/example/introandroidclass/ScrollableActivity.kt
@@ -1,11 +1,34 @@
 package com.example.introandroidclass
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.introandroidclass.model.BoltRide
+import com.example.introandroidclass.model.BoltRideAdapter
 
 class ScrollableActivity : AppCompatActivity() {
+
+    private lateinit var rideListRecyclerView: RecyclerView
+    private lateinit var rideListAdapter: BoltRideAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scrollable)
+        initializeView()
+    }
+
+    private fun getRides(): ArrayList<BoltRide> {
+        val rides = arrayListOf<BoltRide>()
+        rides.add(BoltRide("New garage, Bariga Lagos", "5 Dec, 7:02", "₦ 4,400"))
+        rides.add(BoltRide("Ikeja", "5 Dec, 7:02", "₦ 4,400"))
+        rides.add(BoltRide("Idi Aba, Abeokuta, Ogun State", "5 Dec, 7:02", "₦ 4,400"))
+        return rides
+    }
+
+    private fun initializeView() {
+        rideListRecyclerView = findViewById(R.id.rideList)
+        rideListAdapter = BoltRideAdapter(this, getRides())
+        rideListRecyclerView.adapter = rideListAdapter
+//        rideListRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
