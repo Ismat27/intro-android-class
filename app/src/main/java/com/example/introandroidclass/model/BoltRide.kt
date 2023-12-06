d5705cf74e7a03789fb45387c8ea52efb6b26b1d
ismat27 <adegbiteismail27@gmail.com>
2023-12-06 15:08:00 +0100

    change data structure

--- app/src/main/java/com/example/introandroidclass/model/BoltRide.kt
+++ app/src/main/java/com/example/introandroidclass/model/BoltRide.kt
@@ -1,3 +1,21 @@
 package com.example.introandroidclass.model
 
-data class BoltRide()
+data class BoltRide(
+    val address: String,
+    val date: String,
+    val price: String
+)
+
+val rides = arrayListOf<BoltRide>(
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Ikeja, Lagos", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+    BoltRide("Peugout House, Obafemi Awolowo", "5 Dec, 7:02", "\u20a6 4,400"),
+
+)
--- app/src/main/java/com/example/introandroidclass/model/BoltRideAdapter.kt
+++ app/src/main/java/com/example/introandroidclass/model/BoltRideAdapter.kt
@@ -1,4 +1,39 @@
 package com.example.introandroidclass.model
 
-class BoltRideAdapter {
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.introandroidclass.R
+
+class BoltRideAdapter(private val context: Activity, private val rides: ArrayList<BoltRide>) :
+    RecyclerView.Adapter<BoltRideAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        var rideImageView: ImageView = itemView.findViewById(R.id.rideImage)
+        val rideAddressView: TextView = itemView.findViewById(R.id.rideAddress)
+        val ridePriceView: TextView = itemView.findViewById(R.id.rideAmount)
+        val rideDateView: TextView = itemView.findViewById(R.id.rideDate)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(context).inflate(
+            R.layout.bolt_list_item, parent,
+            false
+        )
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentRideItem = rides[position]
+        holder.rideAddressView.text = currentRideItem.address
+        holder.ridePriceView.text = currentRideItem.price
+        holder.rideDateView.text = currentRideItem.date
+    }
+
+    override fun getItemCount(): Int {
+        return rides.size
+    }
 }
